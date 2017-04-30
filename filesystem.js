0a83bc51fcd046f86a4e214dfe345ee6589cebe1
Adrian Ber <beradrian@yahoo.com>
2017-04-30 20:33:48 +0300

    LocalFileSystem

--- filesystem.js
+++ filesystem.js
@@ -1,4 +1,9 @@
 var Shim = require("./shim.js");
 
+Shim.prefixProperty(window, "requestQuota");
 Shim.prefixProperty(window, "requestFileSystem");
 Shim.prefixProperty(window, "resolveLocalFileSystemURL");
+
+if (!LocalFileSystem) {
+	LocalFileSystem = window;
+}
